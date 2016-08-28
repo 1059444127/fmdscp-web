c563d37e58db920a09406b2857b4f918fc6cb98d
DraconPern <draconpern@hotmail.com>
2016-08-27 23:41:45 -0500

    added get, fixed update

--- routes/destinations.js
+++ routes/destinations.js
@@ -3,15 +3,11 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/', function(req, response, next) {
-  request('http://localhost:8080/api/destinations', function(error, agentresponse, agentbody) { processresult(error, agentresponse, agentbody, req, response) });
+  request('http://localhost:8080/api/destinations', function(error, agentresponse, agentbody) { process_destinations_list(error, agentresponse, agentbody, req, response) });
 });
 
 router.get('/new', function(req, response, next) {
   response.render('destinations/new');
-});
-
-router.get('/update', function(req, response, next) {
-  response.render('destinations/update');
 });
 
 router.post('/new',function(req, response) {
@@ -22,7 +18,11 @@ router.post('/new',function(req, response) {
   }
 });
 
-router.post('/update',function(req, response) {
+router.get('/update/:id', function(req, response, next) {
+  request('http://localhost:8080/api/destinations/' + req.params.id, function(error, agentresponse, agentbody) { process_destinations_get(error, agentresponse, agentbody, req, response) });
+});
+
+router.post('/update/:id',function(req, response) {
   if(req.body.submit == 'Update') {
      update(req, response);
   } else {
@@ -30,14 +30,31 @@ router.post('/update',function(req, response) {
   }
 });
 
-function processresult(error, agentresponse, agentbody, req, response) {
+function process_destinations_list(error, agentresponse, agentbody, req, response) {
   if (!error && agentresponse.statusCode == 200) {
     info = JSON.parse(agentbody);
 
     response.render('destinations/index', { results: info.result});
   } else {
     req.flash('error', 'Failed');
-    response.render('destinations/index', { error: 'query failed'});
+    response.render('destinations/index');
+  }
+}
+
+function process_destinations_get(error, agentresponse, agentbody, req, response) {
+  if (!error && agentresponse.statusCode == 200) {
+    info = JSON.parse(agentbody);
+
+    var name = info.result[0].name;
+    var destinationhost = info.result[0].destinationhost;
+    var destinationport = info.result[0].destinationport;
+    var destinationAE = info.result[0].destinationAE;
+    var sourceAE = info.result[0].sourceAE;
+
+    response.render('destinations/update', { name, destinationhost, destinationport, destinationAE, sourceAE});
+  } else {
+    req.flash('error', 'Failed');
+    response.render('destinations/update');
   }
 }
 
@@ -59,13 +76,11 @@ function add(req, response)
     sourceAE: sourceAE
   }
 
-  require('request').debug = true;
-
   request.post('http://localhost:8080/api/destinations', {form: formData},
     function(error, agentresponse, agentbody) {
       if (!error && agentresponse.statusCode == 200) {
         req.flash('success', 'Added');
-        response.redirect('/destinations/index');
+        response.redirect('/destinations');
       }
       else {
         req.flash('error', 'Query Failed');
@@ -93,21 +108,17 @@ function update(req, response)
     sourceAE: sourceAE
   }
 
-  require('request').debug = true;
-
-  request.post('http://localhost:8080/api/destinations/', {form: formData},
+  request.post('http://localhost:8080/api/destinations/' + req.params.id, {form: formData},
     function(error, agentresponse, agentbody) {
       if (!error && agentresponse.statusCode == 200) {
         req.flash('success', 'Updated');
-        response.redirect('/destinations/index');
+        response.redirect('/destinations');
       }
       else {
         req.flash('error', 'Failed');
-        response.render('destinations/new', { name, destinationhost, destinationport, destinationAE, sourceAE});
+        response.render('destinations/update', { name, destinationhost, destinationport, destinationAE, sourceAE});
       }
     });
 }
-
-
 
 module.exports = router;
