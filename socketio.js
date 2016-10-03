c3b97529bf5709d12e0a87894f8a1a6c5ebfc336
DraconPern <draconpern@hotmail.com>
2016-10-03 01:39:10 -0500

    diferetiate front/backend connection. update frontend w updated info on connect

--- socketio.js
+++ socketio.js
@@ -1,20 +1,44 @@
 var Server = require('socket.io');
-
+var request = require('request');
 var io = new Server();
 
 io.on('connection', (socket) => {
+
+  // two types of connection will come in backend, or web/react
+
+  // they are a backend
   socket.on('agent_auth', (data) => {
+    console.log('backend connected');
     socket.agent_auth = data.access_token;
+
+    // set up to receive log
+    socket.on('log', (data) => {
+        //console.log('log');
+        // console.log(data.context + ":" + data.message);
+    });
+
+    // set up to receive sessions
+    socket.on('updateoutsessionitem', (data) => {
+      // just send it out to everyone
+      io.emit('updateoutsessionitem', data)
+    });
+
   });
 
-  socket.on('log', (data) => {
-      //console.log('log');
-      // console.log(data.context + ":" + data.message);
+// they are a frontend
+  socket.on('front_end', (data) => {
+    console.log('frontend connected');
+    // update status list with the latest
+    request('http://localhost:8080/api/outsessions',
+      function(error, agentresponse, agentbody) {
+        if (!error && agentresponse.statusCode == 200) {
+          info = JSON.parse(agentbody);
+          socket.emit('setoutsessions', info.sessions)
+        }
+      }
+    );
   });
 
-  socket.on('updateoutsessionitem', (data) => {
-    io.emit('updateoutsessionitem', data)
-  });
 });
 
 module.exports = io;
